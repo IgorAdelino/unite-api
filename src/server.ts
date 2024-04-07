5cfce53ae42328889f985a6b1c8b61a12e6adc54
IgorAdelino <igoradelino@gmail.com>
2024-04-06 23:29:29 -0300

    feat: seed test

--- src/server.ts
+++ src/server.ts
@@ -50,5 +50,5 @@ app.register(getEventAttendees)
 app.setErrorHandler(errorHandler)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log("Server listening on port 3000");
+  console.log("Server listening on port 3333");
 })
